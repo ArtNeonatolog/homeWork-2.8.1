ffeb9615797f94d79bf64ba9324b09a8fc504aab
ArtNeonatolog <marcus201@mail.ru>
2022-11-17 22:56:16 +0500

    2 задание (часть 2) выполнено

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Random random = new Random();
+
+        Set<Integer> numbers = new HashSet<>();
+        for (int i = 0; i < 20; i++) {
+            int j = random.nextInt(1000);
+            numbers.add(j);
+            if (j % 2 != 0) {
+                numbers.remove(j);
+            }
+        }
+        System.out.println(numbers);
     }
 }
