990022f2bef40ff4ca96b531bb4fed44d8c22864
ArtNeonatolog <marcus201@mail.ru>
2022-11-21 23:30:31 +0500

    добавлен цикл для удаления нечетных чисел

--- Main.java
+++ Main.java
@@ -1,19 +1,29 @@
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
 
 public class Main {
+
     public static void main(String[] args) {
         Random random = new Random();
 
-        Set<Integer> numbers = new HashSet<>();
+        Set<Integer> numbers = new HashSet<>(20);
         for (int i = 0; i < 20; i++) {
             int j = random.nextInt(1000);
             numbers.add(j);
-            if (j % 2 != 0) {
-                numbers.remove(j);
+        }
+        System.out.println(numbers);
+        System.out.println();
+
+        Iterator<Integer> numberIterator = numbers.iterator();
+        while (numberIterator.hasNext()) {
+            int number = numberIterator.next();
+            if (number % 2 != 0) {
+                numberIterator.remove();
             }
         }
         System.out.println(numbers);
     }
+
 }
